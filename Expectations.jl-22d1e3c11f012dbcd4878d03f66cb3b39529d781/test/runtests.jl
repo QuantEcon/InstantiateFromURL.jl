a1c9ccdb067bb05a6db7a684fb8444d493a89fc8
Arnav Sood <soodarnav01@gmail.com>
2018-10-10 16:26:05 -0700

    remove downloaded tarball

--- Expectations.jl-22d1e3c11f012dbcd4878d03f66cb3b39529d781/test/runtests.jl
+++
@@ -1,9 +0,0 @@
-using Expectations, Distributions
-
-@static if VERSION < v"0.7.0-DEV.2005"
-    using Base.Test, Compat.Random
-else
-    using Test, Random, Statistics, LinearAlgebra
-end
-
-@testset "Iterable distributions" begin include("iterable.jl") end
