aa4da47d9a34f0085c9a5ccfb0b06645eb491073
Arnav Sood <soodarnav01@gmail.com>
2018-10-10 17:27:45 -0700

    a little error handling

--- test/expectations.jl
+++ test/expectations.jl
@@ -8,3 +8,7 @@ activate_github(reponame, version = version)
 activate_github(reponame, version = version, sha = sha)
 
 @test 1 == 1 # Will run if the above things don't break. 
+@test_throws AssertionError activate_github(reponame, sha = "f039628e2b29e2183e8cf41422010d53e65e142") # Wrong length. 
+@test_throws ErrorException activate_github(reponame, sha = "f039628e2b29e2183e8cf41422010d53e65e142d") # Wrong SHA. 
+@test_throws ErrorException activate_github(reponame, version = "v2.0.3") # Version not found. 
+@test_throws ErrorException activate_github("QuantEcon/Expectation.jl", version = "v1.0.0") # Misspelled repo. 
