9a8f227fe443a9afec4ba4a6aeac050ca5b88939
Arnav Sood <soodarnav01@gmail.com>
2018-10-12 10:41:17 -0700

    Pkg and force logic

--- src/activate.jl
+++ src/activate.jl
@@ -1,22 +1,34 @@
 function activate_github(reponame; version = nothing, sha = nothing, force = false)
     # Make sure that our .projects environment is kosher. 
         projdir = joinpath(pwd(), ".projects") # DEPOT_PATH[1] is our .julia 
-        mkpath(projdir) 
+        mkpath(projdir)     
     # For each case of inputs, end up with a concrete URL to download. 
-    if sha != nothing 
-        length(sha) == 40 || throw(ArgumentError("Hash needs to be a 40-character string/hexadecimal number.")) # Check that it's a valid SHA1 hash. 
-        oursha = sha 
-    elseif version != nothing # Given a version but no SHA. 
-        tagobj = tag(reponame, version)
-        oursha = tagobj.object["sha"]
-    else # Download master.
-        oursha = branch(reponame, "master").commit.sha
-    end 
+        if sha != nothing 
+            length(sha) == 40 || throw(ArgumentError("Hash needs to be a 40-character string/hexadecimal number.")) # Check that it's a valid SHA1 hash. 
+            oursha = sha 
+        elseif version != nothing # Given a version but no SHA. 
+            tagobj = tag(reponame, version)
+            oursha = tagobj.object["sha"]
+        else # Download master.
+            oursha = branch(reponame, "master").commit.sha
+        end 
+    # Check if the version is already installed. If it is, activate it. 
+        repostr = split(reponame, "/")[2] # This refers to a git path, not anything local. 
+        ourdir = joinpath(projdir, "$repostr-$oursha")
+        # Try activating and do nothing otherwise. 
+        if isdir(ourdir) && force == false 
+            Pkg.activate(ourdir)
+            pkg"instantiate" 
+            return 0 
+        else
+            # Do nothing. 
+        end 
     # Turn this into a url. 
-    oururl = "https://github.com/$(reponame)/archive/$(oursha).tar.gz"
+        oururl = "https://github.com/$(reponame)/archive/$(oursha).tar.gz"
     # Download that url to projects and unzip. 
-    run(gen_download_cmd(oururl, "$projdir/$oursha.tar.gz"))
-    run(gen_unpack_cmd("$projdir/$oursha.tar.gz", "$projdir")) # Will have package name. 
+        tarpath = joinpath(projdir, "$oursha.tar.gz")
+        run(gen_download_cmd(oururl, tarpath))
+        run(gen_unpack_cmd(tarpath, projdir)) # Will have package name. 
     # Remove the tarball. 
-    rm("$projdir/$oursha.tar.gz")
+        rm("$projdir/$oursha.tar.gz")
 end 
