8d7f61adf6bacc178efea0f4fed88273f817951f
Arnav Sood <soodarnav01@gmail.com>
2018-12-18 13:39:39 -0800

    add TOML stuff

--- src/InstantiateFromURL.jl
+++ src/InstantiateFromURL.jl
@@ -2,7 +2,10 @@ module InstantiateFromURL
 
 # Deps. `using BinaryProvider` needed to instantiate gen_ commands.
 using Pkg, BinaryProvider, Suppressor
-using TOML
+
+# TOML module
+TOMLpath = joinpath(dirname(Sys.BINDIR), "share", "julia", "stdlib", "v1.0", "Pkg", "ext", "TOML")
+include(joinpath(TOMLpath, "src", "TOML.jl"))
 
 # Code
 include("activate.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
+# Standard deps.
 using InstantiateFromURL, Test, Pkg
-using TOML 
 
-@testset begin include("maintest.jl") end # arnavs/InstantationTest
-@testset begin include("tomltest.jl") end 
+# TOML deps.
+TOMLpath = joinpath(dirname(Sys.BINDIR), "share", "julia", "stdlib", "v1.0", "Pkg", "ext", "TOML")
+include(joinpath(TOMLpath, "src", "TOML.jl"))
+
+@testset "Main Tests" begin include("maintest.jl") end # arnavs/InstantationTest
+@testset "TOML Tests" begin include("tomltest.jl") end
