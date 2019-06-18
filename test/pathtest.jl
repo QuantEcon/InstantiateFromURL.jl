e2ac044d2b5ef20c2435733c5236721886526b4e
Arnav Sood <soodarnav01@gmail.com>
2019-06-18 10:32:05 -0700

    update SDO test to account for changes in that repo

--- test/pathtest.jl
+++ test/pathtest.jl
@@ -1,18 +1,18 @@
 reponame = "QuantEcon/SimpleDifferentialOperators.jl"
-path = "docs/examples"
+path = "docs/notebooks"
 
-# vanilla call 
+# vanilla call
 activate_github_path(reponame, path = path, activate = false)
 @test "Project.toml" ∈ readdir()
 @test "Manifest.toml" ∈ readdir()
 
 # vanilla call repeated (should return 0)
-@test_logs (:warn, "There's already a Project.toml in the current directory, and force = false.") activate_github_path(reponame, path = path, force = false, activate = false) 
+@test_logs (:warn, "There's already a Project.toml in the current directory, and force = false.") activate_github_path(reponame, path = path, force = false, activate = false)
 
-# clean 
+# clean
 rm("Project.toml")
 rm("Manifest.toml")
 
-# error handling 
+# error handling
 @test_throws Exception activate_github_path(reponame * "flerg", path, force = false, activate = false)
-@test_throws Exception activate_github_path(reponame, path = "src") # no TOML here 
+@test_throws Exception activate_github_path(reponame, path = "src") # no TOML here
